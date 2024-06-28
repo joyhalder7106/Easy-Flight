64f2524b3d4707d60b4999ea3092cef18b5141d1
joyhalder7106 <teespring.7106@gmail.com>
2024-06-28 17:44:43 +0800

    Air Transfer Function (REST API)

--- app/src/main/java/com/example/easyflight/AirTransferAPI.java
+++ app/src/main/java/com/example/easyflight/AirTransferAPI.java
@@ -77,6 +77,7 @@ import java.net.URL;
         public interface OnAirTransferListener {
             void onAirTransferSuccess(JSONArray flights);
             void onAirTransferError(String errorMessage);
+            // ari transfer
         }
     }
 
